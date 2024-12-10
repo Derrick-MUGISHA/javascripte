931675ddb95a6192b2fb68e2d6e6bcf2d09d231d
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2024-12-11 00:03:42 +0200

    java

---
+++ index.js
@@ -0,0 +1,52 @@
+//fast => second
+//let name ='Mosh';
+//console.log(name);
+
+//cannot be reserved keyword
+//should be meaningful
+//cannot contain a space or hyphen(-)
+//Are case sensitive
+//let interestRate = 0.3;
+//interestRate = 1;
+//console. log(interestRate);
+//strings: primitive string
+//let name ='Mosh'; //string literal
+//let age = 30; // Number literal
+//let isApproved = true; //boolean literal
+//let firstName = undefined;
+//let selectedColor = null;
+
+//let name = {
+  //  name: 'Mosh',
+    //age: 30
+//};
+
+//dot Notation
+//person.name = 'John';
+
+//Bracket Notation
+//person['name'] = 'Mary';
+
+//console.log(person.name);
+
+//arrays// is a date of structure that is used to represent a list of items
+//let selectedColor = ['red', 'green', 'blue'];
+//selectedColor[2] = 1;
+//console.log(selectedColor.length);
+
+
+//functions
+
+function greet(name , lastName) { // perameter is what is at the time of an event
+    console.log('Hello' + name +  '' + lastName);
+}
+
+greet('john' ,'smith');
+greet('marry');// argument is what is passed into the function
+
+//perfromeing a task by function
+function square(number) {
+    return number * number;
+}
+
+ console.log(square(2)); //two function calls
